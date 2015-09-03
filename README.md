6b3c06cd366a4132542abbc2a77306d9f583ff80
Michaël Zasso <mic.besace@gmail.com>
2015-09-03 11:30:47 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # npm-depends
 Find which packages on npm depend on another one
+
+## Installation
+
+`npm install -g npm-depends`
+
+## Usage
+
+`% npm-depends <name> <version>`
+
+The version argument has to be a valid semver number.
+
+## Example
+
+```
+% npm-depends koa 1.0.0
+koa-opinion
+opinion
+riverside
+component-boilerplate-koa
+fitjs
+fora
+glace
+hitchslap
+hive-rest-api
+icejs
+...
+```
+
+## License
+
+[MIT](./LICENSE)
---
+++ npm-depends.js
@@ -0,0 +1,43 @@
+#!/bin/env node
+
+'use strict';
+
+var agent = require('superagent');
+var program = require('commander');
+var semver = require('semver');
+
+var name, version;
+
+program
+    .version(require('./package.json').version)
+    .arguments('<name> <version>')
+    .action(function (name_, version_) {
+        name = name_;
+        version = version_;
+    })
+    .parse(process.argv);
+
+// TODO shouldn't be needed, probably a bug in commander
+if (!name) program.missingArgument('name');
+
+if (!semver.valid(version)) {
+    console.error('invalid version number: ' + version);
+    process.exit(1);
+}
+
+var npmURL = 'https://skimdb.npmjs.com/registry/_design/app/_view/dependentVersions?startkey=%5B%22' + name + '%22%5D&endkey=%5B%22' + name + '!%22%5D&reduce=false';
+agent.get(npmURL)
+    .set('Accept', 'application/json')
+    .end(function (error, response) {
+        if (error) {
+            throw error;
+        }
+        var packages = response.body.rows.filter(function (pack) {
+            return semver.satisfies(version, pack.key[1]);
+        }).map(function (pack) {
+            return pack.id;// + '\t' + pack.key[1];
+        });
+        if (packages.length) {
+            console.log(packages.join('\n'));
+        }
+    });
